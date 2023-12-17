50db592eb4e480bbcd42fd582968fd8e23002082
Thibaultmorizet <thibaultmorizet@people-in.fr>
2023-12-17 21:17:11 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Navigation from "./components/Navigation/Navigation";
+import "./styles/tailwind.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<Router>
+			<Navigation />
+			<Home />
+		</Router>
+	);
 }
-
-export default App;
